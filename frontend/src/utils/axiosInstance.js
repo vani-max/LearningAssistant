ad6319a7d0392ca33b072536c247c77b484f5def
vani <vani.vashishtha2024@nst.rishihood.edu.in>
2026-01-15 19:34:24 +0530

    axios error fixed

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL 
+      || 'https://learningassistant-05fc.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
